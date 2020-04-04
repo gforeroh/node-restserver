b54d65d0d981e83a0e1b92bda435a6cd672f7126
Gerardo forero <gforeroh@gmail.com>
2020-04-04 09:16:01 -0500

    Version 1.1 conexion db

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,29 @@
 // ==================================
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ==================================
+// ENTORNO
+// ==================================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ==================================
+// BASE DE DATOS
+// ==================================
+
+let urlDB;
+const user = 'gerardof';
+const pass = '2gfilhbCUfzNoiPr';
+const db = 'cafe';
+
+if (process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = `mongodb://${user}:${pass}@cluster0-shard-00-00-xdycp.mongodb.net:27017,cluster0-shard-00-01-xdycp.mongodb.net:27017,cluster0-shard-00-02-xdycp.mongodb.net:27017/${db}?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin`;
+    // urlDB = `mongodb+srv://${user}:${pass}@cluster0-xdycp.mongodb.net/${db}`;
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 require('./config/config')
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
 
+
+const app = express();
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
@@ -10,42 +12,33 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json())
+app.use(require('./routes/usuario'))
 
-app.get('/usuario', function (req, res) {
-    res.json('get usuario')
-});
+// console.log(process.env.URLDB);
 
-app.post('/usuario', function (req, res) {
-    let body = req.body;
-    // console.log(body);
 
-    if (body.nombre === undefined){
-        res.status(400).json({
-            ok: false,
-            mensaje: "El nombre es necesario"
-        })
-    } else  {
-        res.json({
-            persona: body
-        })
-    }
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}, (err, res)=> {
+    
+    if(err) throw err;
+
+        console.log("Base de datos ONLINE");
+        // console.log(res);
 
 });
-
-app.put('/usuario/:id', function (req, res) {
-    let id = req.params.id;
-
-    res.json({
-        id
-    })
-});
-
-app.delete('/usuario', function (req, res) {
-    res.json('delete usuario')
-});
-
-
 
 app.listen(process.env.PORT, () => {
     console.log(`Escuchando puerto ${process.env.PORT}`);
 })
+
+// Base de datos MongoDB
+
+// gerardof
+// 2gfilhbCUfzNoiPr
+
+// mongodb://gerardof:2gfilhbCUfzNoiPr@cluster0-shard-00-00-xdycp.mongodb.net:27017,cluster0-shard-00-01-xdycp.mongodb.net:27017,cluster0-shard-00-02-xdycp.mongodb.net:27017/test?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin
+// mongodb+srv://gerardof:2gfilhbCUfzNoiPr@cluster0-xdycp.mongodb.net/admin
